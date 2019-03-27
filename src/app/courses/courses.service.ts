1cb962a9c2022005e2d3936ffeebf11782261a55
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-27 20:03:41 -0300

    done

---
+++ src/app/courses/courses.service.ts
@@ -0,0 +1,9 @@
+export class CoursesService { 
+   getCourses() {
+      return [
+         { name: 'ReactJS crash course!', duration: 2 },
+         { name: 'Learn how to invest your money!', duration: 43 },
+         { name: 'Python for dummies', duration: 54 }
+       ]
+   }
+}
